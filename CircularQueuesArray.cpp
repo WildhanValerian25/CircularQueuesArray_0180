2ba15a617704fc42db6c1c335fb2a11030c763a5
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-06-12 11:16:08 +0700

    Membuat switch case 2

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -136,6 +136,10 @@ int main() {
                 q.insert();
                 break;
             }
+            case '2': {
+                q.remove();
+                break;
+            }
            
     
  
