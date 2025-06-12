b405b0b164c699cbd8f67efaf08e9f2f2ed4029d
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-06-12 10:50:42 +0700

    Membuat fungsi mengecek apakah antrian penuh

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -22,5 +22,13 @@ public:
         cin >> num;
         cout << endl;
 
+        // 1. Cek apakah antrian penuh
+        if ((FRONT + 0 && REAR == max -1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n"; //1.a
+            return; //1.b
+        }
 
+    
+    
  
