f5c225bf4c630be624abb728ef4e8b35e299d1e4
WildhanValerian25 <wildanvalerian89@gmail.com>
2025-06-12 11:11:08 +0700

    Membuat fungsi display iterasi dari awal Array hingga REAR

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -103,6 +103,18 @@ public:
 
                 FRONT_position = 0;
 
-               
+                // Iterasi dari awal array hinggan REAR
+                while (FRONT_position <= REAR_position)
+                {
+                    cout << queue_array[FRONT_position] << "  ";
+                    FRONT_position++;
+                }
+                cout << endl;
+            }
+        }
+    }
+ };
+
+
     
  
